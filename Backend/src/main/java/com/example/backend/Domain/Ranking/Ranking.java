92bdc72f6f95098d612343583bc59a525de7ad4b
eins314 <kang020704@naver.com>
2024-06-02 22:41:40 +0900

    final update plz

--- Backend/src/main/java/com/example/backend/Domain/Ranking/Ranking.java
+++ Backend/src/main/java/com/example/backend/Domain/Ranking/Ranking.java
@@ -1,9 +1,6 @@
 package com.example.backend.Domain.Ranking;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
+import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -17,6 +14,7 @@ import java.util.Map;
 @NoArgsConstructor
 public class Ranking {
 
+    @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
